0aaccdac088abbdd24b7467c1205957aded01659
Max Mathys <threadox@gmail.com>
2016-08-20 00:43:39 +0200

    fuck xamarin forms. xamarin forms is a pain in the ass.

--- HackerNewsXamarinFormsPage.xaml.cs
+++ HackerNewsXamarinFormsPage.xaml.cs
@@ -1,12 +1,40 @@
-﻿using Xamarin.Forms;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xamarin.Forms;
+using System.Net.Http;
 
 namespace HackerNewsXamarinForms
 {
-	public partial class HackerNewsXamarinFormsPage : ContentPage
+	public partial class HackerNewsXamarinFormsPage : INotifyPropertyChanged
 	{
+		public bool IsLoading { get; set; }
 		public HackerNewsXamarinFormsPage()
 		{
+			this.IsLoading = true;
+			this.BindingContext = new[] { "a", "b", "c" };
+
 			InitializeComponent();
+
+			doAsyncWork();
+		}
+
+		private async Task<int> doAsyncWork()
+		{
+			var httpClient = new HttpClient();
+
+			Task<string> contentsTask = httpClient.GetStringAsync("https://hacker-news.firebaseio.com/v0/topstories.json"); // async method!
+
+			string contents = await contentsTask;
+
+			var json = (JObject)JsonConvert.DeserializeObject(contents);
+
+			//TODO
+
+			return contents.Length;
+
 		}
 	}
 }
---
+++ Post.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace HackerNewsXamarinForms
+{
+	public class Post
+	{
+		public string name { get; set;}
+		public string comment { get; set; }
+
+	}
+}
