86d9a9e2524665c4f7d1c16d5aa5df078a0e1bb3
Max Mathys <threadox@gmail.com>
2016-08-22 16:08:44 +0200

    yes haha did it

---
+++ HackerNewsItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Newtonsoft.Json;
+
+
+public class HackerNewsItem
+{
+	public int Id { get; set; }
+	public bool Deleted { get; set; }
+	public string Type { get; set;}
+	public string by { get; set; }
+	public int time { get; set;}
+	public string text { get; set; }
+	public bool dead { get; set; }
+	public object parent { get; set; }
+	public object kids { get; set; }
+	public string url { get; set; }
+	public int score { get; set; }
+	public string title { get; set; }
+	public object parts { get; set; }
+	public int descendants { get; set; }
+	public HackerNewsItem() { }
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Xamarin.Forms;
 
 namespace HackerNewsXamarinForms
@@ -11,12 +15,54 @@ namespace HackerNewsXamarinForms
 	{
 		public ObservableCollection<string> ListViewItems { get; set; }
 		public bool IsLoading { get; private set; }
+		public HttpClient client;
 		public MainPage()
 		{
 			this.IsLoading = true;
 			InitializeComponent();
-			ListViewItems = new ObservableCollection<string> { "one", "two", "three" };
+			ListViewItems = new ObservableCollection<string> {};
 			postsView.ItemsSource = ListViewItems;
+
+			//ListViewItems.Add("hi there testing");
+
+			doPostFetching();
+		}
+
+		async void doPostFetching()
+		{
+			client = new HttpClient();
+			Task<string> getStringTask = client.GetStringAsync("https://hacker-news.firebaseio.com/v0/topstories.json");
+
+			string urlContents = await getStringTask;
+
+			int[] postIds = JsonConvert.DeserializeObject<int[]>(urlContents);
+
+			Debug.WriteLine("before awaiting.");
+
+			await Task.WhenAll(postIds.Select(i => getDetail(this, i)));
+
+			Debug.WriteLine("all completed");
+
+			indicator.SetValue(ActivityIndicator.IsVisibleProperty, false);
+		}
+
+		public async Task<HackerNewsItem> getDetail(MainPage instance, int itemId)
+		{
+			string detailUrl = "https://hacker-news.firebaseio.com/v0/item/" + itemId + ".json";
+			Task<string> getDetailTask = client.GetStringAsync(detailUrl);
+			try
+			{
+				string urlContents = await getDetailTask;
+				HackerNewsItem item = JsonConvert.DeserializeObject<HackerNewsItem>(urlContents);
+				ListViewItems.Add(item.title);
+				return item;
+			}
+#pragma warning disable RECS0022 // A catch clause that catches System.Exception and has an empty body
+			catch (Exception e)
+#pragma warning restore RECS0022 // A catch clause that catches System.Exception and has an empty body
+			{
+				return null;
+			}
 		}
 	}
 }
